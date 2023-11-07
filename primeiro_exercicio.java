3d5a409ab0b7e609ecf1426f8f3d08d519279101
Juliana Sena <julianadsena@gmail.com>
2023-11-07 20:26:43 -0300

    Alterações 07112023

---
+++ primeiro_exercicio.java
@@ -0,0 +1,18 @@
+public class primeiro_exercicio {
+    public static void main(String[] args) {
+        int numero =10;
+        System.out.println(numero);
+        char letra = 'a';
+        System.out.println(letra);
+        short numero2=11;
+        System.out.println(numero2);
+        boolean verdadeiro_falso = true;
+        System.out.println(verdadeiro_falso);
+        String frase;
+        frase = new String("Estou aprendendo java");
+        System.out.println(frase);
+        byte numero3 = 5;
+        System.out.println(numero3);
+
+    }
+}
